6a14c0995da44822f52a43d83fc4454c4234706b
LuisaFC <luisabsb@gmail.com>
2024-08-14 12:02:04 -0300

    contactList header

--- fe/src/components/App/index.js
+++ fe/src/components/App/index.js
@@ -3,13 +3,16 @@ import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 import { Container } from './styles';
 import Header from '../Header';
+import ContactsList from '../ContactsList';
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
+
       <Container>
         <Header />
+        <ContactsList />
       </Container>
     </ThemeProvider>
   );
