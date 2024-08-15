1a454a5eede7a1c63327cecc98e600f6a523bd45
LuisaFC <luisabsb@gmail.com>
2024-08-15 14:08:14 -0300

    refactor header and create home page

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,3 +1,88 @@
+import {
+  Container, Header, ListContainer, Card, InputSearchContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import trash from '../../assets/images/icons/trash.svg';
+import edit from '../../assets/images/icons/edit.svg';
+
 export default function Home() {
-  return 'Homepage';
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato" />
+      </InputSearchContainer>
+      <Header>
+        <strong>3 Contatos</strong>
+        <a href="/">Novo Contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow icon" />
+          </button>
+        </header>
+      </ListContainer>
+
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Luisa</strong>
+            <small>Instagram</small>
+          </div>
+          <span>luisa@teste.com</span>
+          <span>(61) 987765445</span>
+        </div>
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="delete" />
+          </button>
+        </div>
+      </Card>
+
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Luisa</strong>
+            <small>Instagram</small>
+          </div>
+          <span>luisa@teste.com</span>
+          <span>(61) 987765445</span>
+        </div>
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="delete" />
+          </button>
+        </div>
+      </Card>
+
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Luisa</strong>
+            <small>Instagram</small>
+          </div>
+          <span>luisa@teste.com</span>
+          <span>(61) 987765445</span>
+        </div>
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="delete" />
+          </button>
+        </div>
+      </Card>
+
+    </Container>
+  );
 }
