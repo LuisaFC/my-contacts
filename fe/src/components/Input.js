0c7524562ead102d3b34e4c4e93bc114cc9acfb9
LuisaFC <luisabsb@gmail.com>
2024-08-21 13:44:48 -0300

    Button

--- fe/src/components/Input.js
+++ fe/src/components/Input.js
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 export default styled.input`
   width: 100%;
   background: #fff;
-  box-shadow: 0px 4px 10pxrgba(0, 0, 0, 0.4);
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.4);
   height: 52px;
   border-radius: 4px;
   outline: none;
   padding: 0 16px;
   font-size: 16px;
   border: 2px solid #fff;
-  transition: border-colot 0.2s ease-in;
+  transition: border-color 0.2s ease-in;
 
 
   &:focus{
--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,3 +1,4 @@
+import Button from '../../components/Button';
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import Select from '../../components/Select';
@@ -10,6 +11,8 @@ export default function NewContact() {
       <Select>
         <option value="123">Instagram</option>
       </Select>
+      <Button type="button">Salvar</Button>
+      <Button type="button" disabled>Salvar</Button>
     </>
 
   );
