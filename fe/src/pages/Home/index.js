e1733393b96c371a0201bdf90bd8ca772ba10557
LuisaFC <luisabsb@gmail.com>
2025-03-07 13:50:00 -0300

    Error status

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -7,12 +7,11 @@ import arrow from '../../assets/images/icons/arrow.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import edit from '../../assets/images/icons/edit.svg';
 /* import Modal from '../../components/Modal'; */
-import Loader from '../../components/Loader';
+/* import Loader from '../../components/Loader'; */
 
 export default function Home() {
   return (
     <Container>
-      <Loader />
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar contato" />
       </InputSearchContainer>
