6ac1378abe3aee8f6163d3acd5e7b71f6b3b440c
LuisaFC <luisabsb@gmail.com>
2025-03-07 14:34:42 -0300

    Controlled Components

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { Form, ButtonContainer } from './styles';
 import FormGroup from '../FormGroup';
 import Input from '../Input';
@@ -6,10 +7,16 @@ import Select from '../Select';
 import Button from '../Button';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+
   return (
     <Form>
       <FormGroup>
-        <Input placeholder="Nome" />
+        <Input
+          placeholder="Nome"
+          onChange={(event) => setName(event.target.value)}
+          value={name}
+        />
       </FormGroup>
 
       <FormGroup
