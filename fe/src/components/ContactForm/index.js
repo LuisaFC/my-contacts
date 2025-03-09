8d3b08613c7a27cb891828cc420ad589f407d1e9
LuisaFC <luisabsb@gmail.com>
2025-03-09 13:47:06 -0300

    Disabled button condition

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -16,7 +16,11 @@ export default function ContactForm({ buttonLabel }) {
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
 
-  const { setError, removeError, getErrorMessageByFieldName } = useErrors();
+  const {
+    errors, setError, removeError, getErrorMessageByFieldName,
+  } = useErrors();
+
+  const isFormValid = (name && errors.length === 0);
 
   function handleNameChange(event) {
     setName(event.target.value);
@@ -63,6 +67,7 @@ export default function ContactForm({ buttonLabel }) {
       <FormGroup error={getErrorMessageByFieldName('email')}>
         <Input
           type="email"
+          placeholder="Email"
           error={getErrorMessageByFieldName('email')}
           onChange={handleEmailChange}
           value={email}
@@ -90,7 +95,7 @@ export default function ContactForm({ buttonLabel }) {
       </FormGroup>
 
       <ButtonContainer>
-        <Button type="submit">{buttonLabel}</Button>
+        <Button type="submit" disabled={!isFormValid}>{buttonLabel}</Button>
       </ButtonContainer>
     </Form>
   );
