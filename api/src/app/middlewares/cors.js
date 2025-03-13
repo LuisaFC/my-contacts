05d4bb68e00e39a1f945d99c8bc338c48196bba6
LuisaFC <luisabsb@gmail.com>
2025-03-12 22:36:43 -0300

    preflight headers

--- api/src/app/middlewares/cors.js
+++ api/src/app/middlewares/cors.js
@@ -3,5 +3,6 @@ module.exports = ( req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Max-Age', '10');
     next();
 }
