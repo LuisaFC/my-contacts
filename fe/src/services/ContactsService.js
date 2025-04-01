0d1f4fdc1f46f84da1c388ee84f405fc943b18dc
LuisaFC <luisabsb@gmail.com>
2025-03-31 21:08:23 -0300

    correct url

--- fe/src/services/ContactsService.js
+++ fe/src/services/ContactsService.js
@@ -4,7 +4,7 @@ export function ContactsService() {
   const httpClient = HttpClient();
 
   async function listContacts(orderBy = 'asc') {
-    return httpClient.get(`contactss/a3ed4bde-86c1-4aad-9391-6ae3219425db?orderBy=${orderBy}`);
+    return httpClient.get(`contacts/?orderBy=${orderBy}`);
   }
 
   /*  async function createContact(contact) {
