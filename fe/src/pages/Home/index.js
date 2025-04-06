1e5d09edbd5a1a68f01c460f32dd41ed7766fb27
LuisaFC <luisabsb@gmail.com>
2025-04-05 22:49:07 -0300

    Error mesage button click

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-bind */
 import { Link } from 'react-router-dom';
 import { useEffect, useState, useMemo } from 'react';
 import {
@@ -26,21 +27,22 @@ export default function Home() {
     contact.name.toLowerCase().includes(searchTerm.toLowerCase())
   )), [contacts, searchTerm]);
 
-  useEffect(() => {
-    async function loadContacts() {
-      try {
-        setIsLoading(true);
+  async function loadContacts() {
+    try {
+      setIsLoading(true);
 
-        const contactsList = await listContacts(orderBy);
+      const contactsList = await listContacts(orderBy);
 
-        setContacts(contactsList);
-      } catch (error) {
-        setHasError(true);
-      } finally {
-        setIsLoading(false);
-      }
+      setHasError(false);
+      setContacts(contactsList);
+    } catch (error) {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
     }
+  }
 
+  useEffect(() => {
     loadContacts();
   }, [orderBy]);
 
@@ -50,6 +52,10 @@ export default function Home() {
 
   function handleChangeSearchTerm(event) {
     setSearchTerm(event.target.value);
+  }
+
+  function handleTryAgain() {
+    loadContacts();
   }
 
   return (
@@ -72,42 +78,46 @@ export default function Home() {
         <img src={sad} alt="sad" />
         <div className="details">
           <strong>Ocorreu um erro ao obter os seus contatos!</strong>
-          <Button type="button">Tentar Novamente</Button>
+          <Button type="button" onClick={handleTryAgain}>Tentar Novamente</Button>
         </div>
       </ErrorContainer>
       )}
 
-      {filteredContacts.length > 0 && (
-        <ListHeader orderBy={orderBy}>
-          <button type="button" onClick={handleToggleOrderBy}>
-            <span>Nome</span>
-            <img src={arrow} alt="Arrow icon" />
-          </button>
-        </ListHeader>
-      )}
-
-      {filteredContacts.map((contact) => (
-        <Card key={contact.id}>
-          <div className="info">
-            <div className="contact-name">
-              <strong>{contact.name}</strong>
-              {contact.category_name && (
-                <small>{contact.category_name}</small>
-              )}
-            </div>
-            <span>{contact.email}</span>
-            <span>{contact.phone}</span>
-          </div>
-          <div className="actions">
-            <Link to={`/edit/${contact.id}`}>
-              <img src={edit} alt="edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="delete" />
+      {!hasError && (
+        <>
+          {filteredContacts.length > 0 && (
+          <ListHeader orderBy={orderBy}>
+            <button type="button" onClick={handleToggleOrderBy}>
+              <span>Nome</span>
+              <img src={arrow} alt="Arrow icon" />
             </button>
-          </div>
-        </Card>
-      ))}
+          </ListHeader>
+          )}
+
+          {filteredContacts.map((contact) => (
+            <Card key={contact.id}>
+              <div className="info">
+                <div className="contact-name">
+                  <strong>{contact.name}</strong>
+                  {contact.category_name && (
+                  <small>{contact.category_name}</small>
+                  )}
+                </div>
+                <span>{contact.email}</span>
+                <span>{contact.phone}</span>
+              </div>
+              <div className="actions">
+                <Link to={`/edit/${contact.id}`}>
+                  <img src={edit} alt="edit" />
+                </Link>
+                <button type="button">
+                  <img src={trash} alt="delete" />
+                </button>
+              </div>
+            </Card>
+          ))}
+        </>
+      )}
 
     </Container>
   );
--- fe/src/services/utils/endpoints.js
+++ fe/src/services/utils/endpoints.js
@@ -1,1 +1,1 @@
-export const LOCALHOST = 'http://localhost:3001';
+export const LOCALHOST = 'http://localhost:3002';
